c60f96dcc66ea50f001af808411420b85caf597e
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-06-19 16:24:26 +0900

    [KNK-159] Feat: 스토리 완성 화면 로딩 스켈레톤 레이아웃 수정

--- src/features/stories/new/components/story-completion-section.tsx
+++ src/features/stories/new/components/story-completion-section.tsx
@@ -18,29 +18,49 @@ type StoryCompletionSectionProps = {
 function StoryCompletionLoadingState() {
   return (
     <div className="flex flex-col gap-8" aria-label="스토리 완성 중">
-      <div className="flex flex-col gap-2">
-        <Skeleton className="w-1.5/12 h-3.5" />
-        <Skeleton className="h-4 w-5/12" />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-3.5 w-2/12" />
-        <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-8 w-7/12" />
+          <div className="flex gap-1">
+            <Skeleton className="h-5 w-12 rounded-full" />
+            <Skeleton className="h-5 w-16 rounded-full" />
+            <Skeleton className="h-5 w-14 rounded-full" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
           <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-2/12" />
+          <Skeleton className="h-4 w-8/12" />
         </div>
       </div>
 
       <div className="flex flex-col gap-2">
-        <Skeleton className="h-3.5 w-2/12" />
-        <div className="flex flex-col gap-1">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-4/12" />
-          <Skeleton className="h-4 w-0" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-5/12" />
-          <Skeleton className="h-4 w-0" />
-          <Skeleton className="h-4 w-11/12" />
+        <Skeleton className="h-7 w-3/12" />
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-7/12" />
+          </div>
+          <div>
+            <Skeleton className="h-4 w-11/12" />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-3/12" />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-7 w-3/12" />
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-3.5 w-2/12" />
+            <Skeleton className="h-10 w-full rounded-md" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-3.5 w-2/12" />
+            <Skeleton className="h-20 w-full rounded-md" />
+          </div>
         </div>
       </div>
     </div>
