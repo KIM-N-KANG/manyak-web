60b3dd1499037960a7aee05f3bccd8c527305da0
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-07-23 17:05:44 +0900

    [KNK-677] Fix: react-grab 오버레이가 E2E 클릭을 막던 문제 해결
    - 개발 모드 자동 로드를 REACT_GRAB=1 opt-in으로 변경
    - 그랩을 켜서 실행하는 dev:grab 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,13 +58,16 @@ export default function RootLayout({
       className={`${pretendard.variable} ${maruburi.variable} antialiased`}
       suppressHydrationWarning>
       <head>
-        {process.env.NODE_ENV === 'development' && (
-          <Script
-            src="//unpkg.com/react-grab/dist/index.global.js"
-            crossOrigin="anonymous"
-            strategy="beforeInteractive"
-          />
-        )}
+        {/* react-grab은 전체 화면 오버레이를 깔아 포인터 이벤트를 가로채므로 E2E(dev 서버 사용)의 클릭을 모두 막는다.
+            그래서 개발 모드에서도 자동 로드하지 않고 `pnpm dev:grab`으로 명시적으로 켤 때만 주입한다. */}
+        {process.env.NODE_ENV === 'development' &&
+          process.env.REACT_GRAB === '1' && (
+            <Script
+              src="//unpkg.com/react-grab/dist/index.global.js"
+              crossOrigin="anonymous"
+              strategy="beforeInteractive"
+            />
+          )}
       </head>
       {/* suppressHydrationWarning: 카카오톡 iOS 웹뷰가 body에 -webkit-text-size-adjust 스타일을 주입해 속성 불일치 경고가 발생 */}
       <body
